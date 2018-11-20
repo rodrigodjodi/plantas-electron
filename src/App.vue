e9196a47aec5a0035eeb1471cc00daae9f7953f0
rodrigodjodi <rodrigo.djodi@gmail.com>
2018-11-20 19:17:42 -0200

    Uso do componente de planta, estado.

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,82 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>ACMA</span>
+        <span class="font-weight-light">Prudente de Moraes</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+        @click="makePDF"
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">salvar pdf</span>
       </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container fluid>
+        <v-layout justify-center >
+          <v-flex xs12 class="text-xs-center">
+            <v-btn-toggle v-model="imageFile" mandatory>
+              <v-btn flat value="subsolo1.jpg">
+                Subsolo 1
+              </v-btn>
+              <v-btn flat value="subsolo2.jpg">
+                Subsolo 2
+              </v-btn>
+            </v-btn-toggle>
+          </v-flex>
+        </v-layout>
+        <Planta :imageFile="imageFile"/>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Planta from "./components/Planta";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Planta
   },
-  data () {
+  data() {
     return {
-      //
+      imageFile: "subsolo1.jpg",
+      unidades: [
+        "101",
+        "201",
+        "202",
+        "203",
+        "204",
+        "301",
+        "302",
+        "303",
+        "304",
+        "401",
+        "402",
+        "403",
+        "404",
+        "501",
+        "502",
+        "503",
+        "504",
+        "601",
+        "602",
+        "603",
+        "604",
+        "701",
+        "702",
+        "703",
+        "704"
+      ]
+    };
+  },
+  methods: {
+    makePDF() {
+      alert("Não implementado!");
     }
   }
-}
+};
 </script>

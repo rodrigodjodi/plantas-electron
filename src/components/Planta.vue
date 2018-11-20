871e7235eee4e54a87d4195fcdf13e5ae4c00f07
rodrigodjodi <rodrigo.djodi@gmail.com>
2018-11-20 19:17:58 -0200

    Base do componente de planta.

---
+++ src/components/Planta.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-layout>
+      <v-flex xs12>
+        <v-img
+          :src="require(`../assets/${imageFile}`)"
+          contain
+          max-height="800"
+        ></v-img>
+      </v-flex>
+    </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    imageFile: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>

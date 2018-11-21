60ddfff8b5c39a0cab7df3faa4304ff291240f51
rodrigodjodi <rodrigo.djodi@gmail.com>
2018-11-21 19:06:06 -0200

    Junção dos arquivos de vagas.

---
+++ src/assets/vagas.js
@@ -0,0 +1,668 @@
+export default [
+  {
+    numero: "01",
+    nivel: "subsolo1",
+    ap: "101",
+    x: 17,
+    y: 74,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "02",
+    nivel: "subsolo1",
+    ap: null,
+    x: 17,
+    y: 190,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "03",
+    nivel: "subsolo1",
+    ap: null,
+    x: 17,
+    y: 275,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "04",
+    nivel: "subsolo1",
+    ap: null,
+    x: 17,
+    y: 362,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "05",
+    nivel: "subsolo1",
+    ap: null,
+    x: 17,
+    y: 440,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "06",
+    nivel: "subsolo1",
+    ap: null,
+    x: 17,
+    y: 518,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "07",
+    nivel: "subsolo1",
+    ap: null,
+    x: 17,
+    y: 605,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "08",
+    nivel: "subsolo1",
+    ap: null,
+    x: 17,
+    y: 691,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "09",
+    nivel: "subsolo1",
+    ap: null,
+    x: 17,
+    y: 818,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "10",
+    nivel: "subsolo1",
+    ap: null,
+    x: 168,
+    y: 781,
+    width: 64,
+    height: 136
+  },
+  {
+    numero: "11",
+    nivel: "subsolo1",
+    ap: null,
+    x: 246,
+    y: 781,
+    width: 64,
+    height: 136
+  },
+  {
+    numero: "12",
+    nivel: "subsolo1",
+    ap: null,
+    x: 348,
+    y: 781,
+    width: 64,
+    height: 136
+  },
+  {
+    numero: "13",
+    nivel: "subsolo1",
+    ap: null,
+    x: 425,
+    y: 781,
+    width: 64,
+    height: 136
+  },
+  {
+    numero: "14",
+    nivel: "subsolo1",
+    ap: null,
+    x: 960,
+    y: 598,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "15",
+    nivel: "subsolo1",
+    ap: null,
+    x: 960,
+    y: 517,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "16",
+    nivel: "subsolo1",
+    ap: null,
+    x: 960,
+    y: 441,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "17",
+    nivel: "subsolo1",
+    ap: null,
+    x: 960,
+    y: 364,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "18",
+    nivel: "subsolo1",
+    ap: null,
+    x: 960,
+    y: 277,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "19",
+    nivel: "subsolo1",
+    ap: null,
+    x: 960,
+    y: 192,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "20",
+    nivel: "subsolo1",
+    ap: null,
+    x: 960,
+    y: 64,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "21",
+    nivel: "subsolo1",
+    ap: null,
+    x: 699,
+    y: 64,
+    width: 121,
+    height: 64
+  },
+  {
+    numero: "22",
+    nivel: "subsolo1",
+    ap: null,
+    x: 699,
+    y: 184,
+    width: 121,
+    height: 64
+  },
+  {
+    numero: "23",
+    nivel: "subsolo1",
+    ap: null,
+    x: 684,
+    y: 259,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "24",
+    nivel: "subsolo1",
+    ap: null,
+    x: 684,
+    y: 336,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "25",
+    nivel: "subsolo1",
+    ap: null,
+    x: 727,
+    y: 512,
+    width: 64,
+    height: 122
+  },
+  {
+    numero: "26",
+    nivel: "subsolo1",
+    ap: null,
+    x: 557,
+    y: 574,
+    width: 150,
+    height: 64
+  },
+  {
+    numero: "27",
+    nivel: "subsolo1",
+    ap: null,
+    x: 455,
+    y: 512,
+    width: 65,
+    height: 122
+  },
+  {
+    numero: "28",
+    nivel: "subsolo1",
+    ap: null,
+    x: 360,
+    y: 495,
+    width: 57,
+    height: 137
+  },
+  {
+    numero: "29",
+    nivel: "subsolo1",
+    ap: null,
+    x: 293,
+    y: 496,
+    width: 63,
+    height: 136
+  },
+  {
+    numero: "30",
+    nivel: "subsolo1",
+    ap: null,
+    x: 293,
+    y: 315,
+    width: 63,
+    height: 136
+  },
+  {
+    numero: "31",
+    nivel: "subsolo1",
+    ap: null,
+    x: 360,
+    y: 314,
+    width: 57,
+    height: 137
+  },
+  {
+    numero: "32",
+    nivel: "subsolo1",
+    ap: "702",
+    x: 455,
+    y: 314,
+    width: 65,
+    height: 122
+  },
+  {
+    numero: "33",
+    nivel: "subsolo1",
+    ap: null,
+    x: 574,
+    y: 184,
+    width: 121,
+    height: 64
+  },
+  {
+    numero: "34",
+    nivel: "subsolo1",
+    ap: null,
+    x: 574,
+    y: 64,
+    width: 121,
+    height: 64
+  },
+  {
+    numero: "35",
+    nivel: "subsolo1",
+    ap: null,
+    x: 492,
+    y: 38,
+    width: 64,
+    height: 138
+  },
+  {
+    numero: "36",
+    nivel: "subsolo1",
+    ap: null,
+    x: 425,
+    y: 38,
+    width: 64,
+    height: 138
+  },
+  {
+    numero: "37",
+    nivel: "subsolo1",
+    ap: null,
+    x: 325,
+    y: 38,
+    width: 64,
+    height: 138
+  },
+  {
+    numero: "38",
+    nivel: "subsolo1",
+    ap: null,
+    x: 224,
+    y: 38,
+    width: 64,
+    height: 138
+  },
+  {
+    numero: "39",
+    nivel: "subsolo1",
+    ap: null,
+    x: 157,
+    y: 38,
+    width: 64,
+    height: 138
+  },
+  {
+    numero: "40",
+    nivel: "subsolo2",
+    ap: null,
+    x: 17,
+    y: 843,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "41",
+    nivel: "subsolo2",
+    ap: null,
+    x: 17,
+    y: 781,
+    width: 138,
+    height: 58
+  },
+  {
+    numero: "42",
+    nivel: "subsolo2",
+    ap: null,
+    x: 17,
+    y: 691,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "43",
+    nivel: "subsolo2",
+    ap: null,
+    x: 17,
+    y: 605,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "44",
+    nivel: "subsolo2",
+    ap: null,
+    x: 17,
+    y: 518,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "45",
+    nivel: "subsolo2",
+    ap: null,
+    x: 17,
+    y: 440,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "46",
+    nivel: "subsolo2",
+    ap: null,
+    x: 17,
+    y: 362,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "47",
+    nivel: "subsolo2",
+    ap: null,
+    x: 17,
+    y: 275,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "48",
+    nivel: "subsolo2",
+    ap: null,
+    x: 17,
+    y: 190,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "49",
+    nivel: "subsolo2",
+    ap: null,
+    x: 17,
+    y: 74,
+    width: 138,
+    height: 65
+  },
+  {
+    numero: "50",
+    nivel: "subsolo2",
+    ap: null,
+    x: 157,
+    y: 38,
+    width: 64,
+    height: 138
+  },
+  {
+    numero: "51",
+    nivel: "subsolo2",
+    ap: null,
+    x: 224,
+    y: 38,
+    width: 64,
+    height: 138
+  },
+  {
+    numero: "52",
+    nivel: "subsolo2",
+    ap: null,
+    x: 325,
+    y: 38,
+    width: 64,
+    height: 138
+  },
+  {
+    numero: "53",
+    nivel: "subsolo2",
+    ap: null,
+    x: 425,
+    y: 38,
+    width: 64,
+    height: 138
+  },
+  {
+    numero: "54",
+    nivel: "subsolo2",
+    ap: null,
+    x: 492,
+    y: 38,
+    width: 64,
+    height: 138
+  },
+  {
+    numero: "55",
+    nivel: "subsolo2",
+    ap: null,
+    x: 574,
+    y: 64,
+    width: 121,
+    height: 64
+  },
+  {
+    numero: "56",
+    nivel: "subsolo2",
+    ap: null,
+    x: 699,
+    y: 64,
+    width: 121,
+    height: 64
+  },
+  {
+    numero: "57",
+    nivel: "subsolo2",
+    ap: null,
+    x: 960,
+    y: 64,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "58",
+    nivel: "subsolo2",
+    ap: null,
+    x: 960,
+    y: 192,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "59",
+    nivel: "subsolo2",
+    ap: null,
+    x: 960,
+    y: 277,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "60",
+    nivel: "subsolo2",
+    ap: null,
+    x: 960,
+    y: 364,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "61",
+    nivel: "subsolo2",
+    ap: null,
+    x: 960,
+    y: 441,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "62",
+    nivel: "subsolo2",
+    ap: null,
+    x: 960,
+    y: 517,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "63",
+    nivel: "subsolo2",
+    ap: null,
+    x: 960,
+    y: 606,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "64",
+    nivel: "subsolo2",
+    ap: null,
+    x: 960,
+    y: 694,
+    width: 136,
+    height: 64
+  },
+  {
+    numero: "65",
+    nivel: "subsolo2",
+    ap: null,
+    x: 853,
+    y: 781,
+    width: 65,
+    height: 136
+  },
+  {
+    numero: "66",
+    nivel: "subsolo2",
+    ap: null,
+    x: 727,
+    y: 512,
+    width: 64,
+    height: 122
+  },
+  {
+    numero: "67",
+    nivel: "subsolo2",
+    ap: null,
+    x: 557,
+    y: 574,
+    width: 150,
+    height: 64
+  },
+  {
+    numero: "68",
+    nivel: "subsolo2",
+    ap: null,
+    x: 455,
+    y: 512,
+    width: 65,
+    height: 122
+  },
+  {
+    numero: "69",
+    nivel: "subsolo2",
+    ap: null,
+    x: 360,
+    y: 495,
+    width: 57,
+    height: 137
+  },
+  {
+    numero: "70",
+    nivel: "subsolo2",
+    ap: null,
+    x: 293,
+    y: 496,
+    width: 63,
+    height: 136
+  },
+  {
+    numero: "71",
+    nivel: "subsolo2",
+    ap: null,
+    x: 293,
+    y: 315,
+    width: 63,
+    height: 136
+  },
+  {
+    numero: "72",
+    nivel: "subsolo2",
+    ap: null,
+    x: 360,
+    y: 314,
+    width: 57,
+    height: 137
+  },
+  {
+    numero: "73",
+    nivel: "subsolo2",
+    ap: null,
+    x: 455,
+    y: 314,
+    width: 65,
+    height: 122
+  },
+  {
+    numero: "74",
+    nivel: "subsolo2",
+    ap: null,
+    x: 680,
+    y: 273,
+    width: 99,
+    height: 136
+  }
+];

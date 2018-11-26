347e693ed2d9aade9537542738705a857a10939b
rodrigodjodi <rodrigo.djodi@gmail.com>
2018-11-26 18:10:59 -0200

    Firebase db saiu de main.js para planta.vue.

--- src/components/Planta.vue
+++ src/components/Planta.vue
@@ -1,60 +1,62 @@
 <template>
   <v-layout wrap>
     <v-flex xs12 lg10>
-      <v-img
-        :src="require(`../assets/${nivel}.jpg`)"
-        contain
-        max-height="800"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg"
+      <v-img class="printBG" :src="require(`../assets/${nivel}.jpg`)" contain max-height="800">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
           xmlns:xlink="http://www.w3.org/1999/xlink"
-          preserveAspectRatio="xMidYMid" width="100%"
+          preserveAspectRatio="xMidYMid"
+          width="100%"
           viewBox="0 0 1600 938"
         >
           <defs>
-            <pattern id="hachura" width="5" height="5"
+            <pattern
+              id="hachura"
+              width="5"
+              height="5"
               patternTransform="rotate(45 0 0)"
-              patternUnits="userSpaceOnUse">
-              <line x1="0" y1="0" x2="0" y2="10" 
-                style="stroke:orange; stroke-width:3"
-              />
+              patternUnits="userSpaceOnUse"
+            >
+              <line x1="0" y1="0" x2="0" y2="10" style="stroke:orange; stroke-width:3"></line>
             </pattern>
-           </defs>
-          <rect v-for="vaga in vagas" :key="vaga.numero"
-            :x="vaga.x" :y="vaga.y"
-            :width="vaga.width" :height="vaga.height"
+          </defs>
+          <rect
+            v-for="vaga in vagas"
+            :key="vaga.numero"
+            :x="vaga.x"
+            :y="vaga.y"
+            :width="vaga.width"
+            :height="vaga.height"
             :class="{'vaga-hl': highLighted==vaga.numero, 'vaga-ocupada': vaga.ap }"
-            opacity="0" fill="url(#hachura)"
+            opacity="0"
+            fill="url(#hachura)"
             @mouseover="onMouseover(vaga.numero)"
             @mouseleave="onMouseover(0)"
             @click="showDialog(vaga)"
-          />
-          <text v-for="vaga  in vagas" :key="'v'+vaga.numero"
-            :x="vaga.x + (vaga.width/2)" :y="vaga.y + (vaga.height/2)"
-            class="txt-ap" dy="1rem"
-          >
-            {{vaga.ap || ""}}
-          </text>
+          ></rect>
+          <text
+            v-for="vaga  in vagas"
+            :key="'v'+vaga.numero"
+            :x="vaga.x + (vaga.width/2)"
+            :y="vaga.y + (vaga.height/2)"
+            class="txt-ap"
+            dy="1rem"
+          >{{vaga.ap || ""}}</text>
         </svg>
       </v-img>
     </v-flex>
     <v-flex xs12 lg2>
-      <v-data-table hide-actions 
-          :items="vagas" :headers="headers"
-          class="elevation-1"
-        >
-          <template slot="items" slot-scope="props">
-            <td 
-              :class="{'hl': highLighted==props.item.numero}"
-              @mouseover="onMouseover(props.item.numero)"
-              @mouseleave="onMouseover(0)"
-              @click="showDialog(props.item)"
-            >{{ props.item.numero }}</td>
-            <td class="text-xs-right">
-              {{props.item.ap}}
-            </td>
-          </template>
-        </v-data-table>
+      <v-data-table hide-actions :items="vagas" :headers="headers" class="elevation-1">
+        <template slot="items" slot-scope="props">
+          <td
+            :class="{'hl': highLighted==props.item.numero}"
+            @mouseover="onMouseover(props.item.numero)"
+            @mouseleave="onMouseover(0)"
+            @click="showDialog(props.item)"
+          >{{ props.item.numero }}</td>
+          <td class="text-xs-right">{{props.item.ap}}</td>
+        </template>
+      </v-data-table>
     </v-flex>
     <v-dialog v-model="dialog" max-width="320px">
       <v-card>
@@ -66,14 +68,14 @@
           <v-container>
             <v-layout>
               <v-flex xs12>
-                <v-select v-model="dialogAp"
+                <v-select
+                  v-model="dialogAp"
                   :items="unidades"
                   label="Escolha um apartamento da lista..."
                   solo
                   clearable
                 ></v-select>
               </v-flex>
-              
             </v-layout>
           </v-container>
         </v-card-text>
@@ -86,11 +88,17 @@
       </v-card>
     </v-dialog>
   </v-layout>
-  
 </template>
 
 <script>
-import db from "../main";
+import firebase from "@firebase/app";
+import "@firebase/auth";
+import "@firebase/firestore";
+import config from "../assets/firebaseConfig";
+firebase.initializeApp(config);
+firebase.auth().signInAnonymously();
+const db = firebase.firestore();
+db.settings({ timestampsInSnapshots: true });
 export default {
   props: {
     nivel: {
@@ -103,15 +111,15 @@ export default {
       highLighted: "",
       todasAsVagas: [],
       unidades: [],
-      dialog:false,
+      dialog: false,
       headers: [
         {
-          text: 'Vaga',
-          align: 'left',
+          text: "Vaga",
+          align: "left",
           sortable: false,
-          value: 'vaga'
+          value: "vaga"
         },
-        { text: 'Apartamento', value: 'ap', sortable: false },
+        { text: "Apartamento", value: "ap", sortable: false }
       ],
       editandoVaga: 0,
       dialogHeadline: "",
@@ -134,10 +142,10 @@ export default {
     showDialog(ev) {
       this.editandoVaga = ev.numero;
       if (ev.ap === null) {
-        this.dialogConfirmText = "Reservar"
-        this.dialogHeadline = `Reservar vaga ${ev.numero}?`
+        this.dialogConfirmText = "Reservar";
+        this.dialogHeadline = `Reservar vaga ${ev.numero}?`;
       } else {
-        this.dialogConfirmText = "Alterar"
+        this.dialogConfirmText = "Alterar";
         this.dialogHeadline = `Alterar vaga ${ev.numero}?`;
         this.dialogAp = ev.ap;
       }
@@ -151,18 +159,19 @@ export default {
       this.dialog = false;
     },
     save() {
-      if (typeof this.dialogAp === 'undefined') {
+      if (typeof this.dialogAp === "undefined") {
         this.dialogAp = null;
-      };
+      }
       db.doc(`projetos/prudente/vagas/${this.editandoVaga}`)
-      .update({ap: this.dialogAp}).then(()=> {
-        console.log("Document successfully updated!");
-        this.closeDialog();
-      })
-      .catch(function(error) {
+        .update({ ap: this.dialogAp })
+        .then(() => {
+          console.log("Document successfully updated!");
+          this.closeDialog();
+        })
+        .catch(function(error) {
           // The document probably doesn't exist.
-        console.error("Error updating document: ", error);
-      });
+          console.error("Error updating document: ", error);
+        });
     }
   },
   created() {
@@ -199,6 +208,10 @@ td.hl {
 }
 td {
   height: 18px !important;
+}
+.printBG {
+  -webkit-print-color-adjust: exact !important;   /* Chrome, Safari */
+  color-adjust: exact !important;
 }
 .select-ap {
   padding: 0;
--- src/main.js
+++ src/main.js
@@ -2,21 +2,10 @@ import Vue from "vue";
 import "./plugins/vuetify";
 import App from "./App.vue";
 import "./registerServiceWorker";
-import firebase from "@firebase/app";
-import "@firebase/auth";
-import "@firebase/firestore";
-import config from "./assets/firebaseConfig";
-firebase.initializeApp(config);
+
 Vue.config.productionTip = false;
-firebase.auth().signInAnonymously();
-let app;
-if (!app) {
-  app = new Vue({
-    render: function(h) {
-      return h(App);
-    }
-  }).$mount("#app");
-}
-const db = firebase.firestore();
-db.settings({ timestampsInSnapshots: true });
-export default db;
+new Vue({
+  render: function(h) {
+    return h(App);
+  }
+}).$mount("#app");
